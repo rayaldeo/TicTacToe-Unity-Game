92ea8433d09cd82e87415d6d3c7a3af5fe7ee7f8
Jonathan Elliott <jn288377@dal.ca>
2020-01-18 16:02:08 -0500

    Working on TicTacToeEngine

--- Assets/Scripts/TicTacToeEngine.cs
+++ Assets/Scripts/TicTacToeEngine.cs
@@ -6,6 +6,8 @@ public class TicTacToeEngine : MonoBehaviour
 {
 
     protected GameObjectState blank = new Blank();
+    int bestMove = -1;
+    int bestLocation = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,34 @@ public class TicTacToeEngine : MonoBehaviour
     {
         Debug.Log("I am thinking..beep...bap...boop");
         Debug.Log("AI:"+board.transform.Find("GameObjects").transform.childCount);
-        Debug.Log("Enabling sector 3");
-        board.transform.Find("GameObjects").transform.GetChild(2).gameObject.GetComponent<SectorSystem>().Place_O();
+        //Debug.Log("Enabling sector 3");
+        //board.transform.Find("GameObjects").transform.GetChild(2).gameObject.GetComponent<SectorSystem>().Place_O();
+        IAmThinkingRight(board);
+    }
+
+
+    public void IAmThinkingRight(GameObject board)
+    {
+        for(int i=0;i< board.transform.Find("GameObjects").transform.childCount;i++)
+        {
+            Debug.Log("Index:" + i);
+            Transform sector = board.transform.Find("GameObjects").transform.GetChild(i);
+            if (sector.GetComponent<SectorSystem>().GetState().Equals(blank.ToString()))
+            {
+                int score = MiniMax(board);
+                if (score > bestMove)
+                {
+                    bestMove = score;
+                    bestLocation = i;
+                    sector.GetComponent<SectorSystem>().Place_O();
+                }
+            }
+        }
+        bestMove = -1;
+    }
+
+    int MiniMax(GameObject board)
+    {
+        return 1;
     }
 }
